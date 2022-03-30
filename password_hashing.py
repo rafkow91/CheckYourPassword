b17920f7295493c44ddd6163d7d4cf643690da56
Rafał Kowalski <rafkow91@gmail.com>
2022-03-30 22:48:07 +0200

    refactor password_hashing class

--- password_hashing.py
+++ password_hashing.py
@@ -1,18 +1,17 @@
 from hashlib import sha1
 from abstractive_classes import Validator
 
-URL_ADDRESS = 'https://api.pwnedpasswords.com/range/'
+
 
 
 class HashPassword(Validator):
-    def __init__(self, password) -> None:
+    def __init__(self, password: str, hashed_password: str = None) -> None:
         self.password = password
-        self.hashed_password = None
+        self.hashed_password = hashed_password
 
     def hash_password(self):
-        hashed_password = sha1(self.password.encode())
-        self.hashed_password = hashed_password.hexdigest()
-        return self.hashed_password
+        self.hashed_password = sha1(self.password.encode()).hexdigest()
+        return self.hash_password
 
     def validate(self):
         try:
